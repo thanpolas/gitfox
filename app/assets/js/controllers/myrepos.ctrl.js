b4a5302f0a34a44e054d6f40a67744262a6efb7b
Thanasis Polychronakis <thanpolas@gmail.com>
2013-01-26 15:32:57 +0200

    added my repos page

---
+++ app/assets/js/controllers/myrepos.ctrl.js
@@ -0,0 +1,11 @@
+var gitfox = gitfox || {};
+gitfox.ctrl = gitfox.ctrl || {};
+
+
+!function(gitfox) {
+  gitfox.ctrl.myRepos = function myRepos($scope, $routeParams) {
+    log($scope);
+    log($routeParams);
+  };
+
+}(gitfox);
--- app/assets/js/gitfox.js
+++ app/assets/js/gitfox.js
@@ -7,7 +7,8 @@
     config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/', {templateUrl: 'partials/frontpage.html',   controller: gitfox.ctrl.frontpage}).
-        when('/mygists', {templateUrl: 'partials/mygists.html',   controller: gitfox.ctrl.mygists}).
+        when('/mygists', {templateUrl: 'partials/mygists.html',   controller: gitfox.ctrl.myGists}).
+        when('/myrepos', {templateUrl: 'partials/myrepos.html',   controller: gitfox.ctrl.myRepos}).
         otherwise({redirectTo: '/'});
   }]);
 
